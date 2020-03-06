dfa355aca9563e73112740614a47ce920bf0c4cd
Julius Kariuki Magondu <kariukijulius63@gmail.com>
2020-03-06 09:29:42 +0300

    added nodemon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Router, Route, Switch } from "react-router-dom";
-import "./custom.scss";
 import { createBrowserHistory } from "history";
 const history = createBrowserHistory();
 
